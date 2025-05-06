513c7cd1439d62264d12a641792c911e3ecf619f
Francisco Javier Cuena Serrano <fjcuenase@gmail.com>
2025-05-06 20:22:46 +0200

    feat: lines to branches added

--- src/app/shared/concepts/decision-node/decision-node.component.ts
+++ src/app/shared/concepts/decision-node/decision-node.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
   QueryList,
   ViewChildren,
+  OnInit
 } from '@angular/core';
 import { DecisionNode } from './models/decision-node.model';
 import { gsap } from 'gsap';
@@ -21,10 +22,19 @@ export class DecisionNodeComponent implements AfterViewInit {
   @Input() node!: DecisionNode;
 
   @ViewChild('nodeRef', { static: true }) nodeRef!: ElementRef;
+  @ViewChild('container', { static: true }) containerRef!: ElementRef;
   @ViewChildren(DecisionNodeComponent)
-  childNodes!: QueryList<DecisionNodeComponent>;
+  childrenNodes!: QueryList<DecisionNodeComponent>;
+
+  selfCoord = { x: 0, y: 0 };
+  childCoords: { x: number; y: number }[] = [];
 
   ngAfterViewInit(): void {
+    this.animateSelf();
+    setTimeout(() => this.updateLinePositions(), 0); // wait for children to render
+  }
+
+  animateSelf() {
     gsap.fromTo(
       this.nodeRef.nativeElement,
       { scale: 0, opacity: 0 },
@@ -36,5 +46,25 @@ export class DecisionNodeComponent implements AfterViewInit {
         delay: this.node.level * 0.3,
       }
     );
+  }
+  updateLinePositions() {
+    const nodeEl = this.nodeRef.nativeElement as HTMLElement;
+    const containerEl = this.containerRef.nativeElement as HTMLElement;
+
+    const nodeRect = nodeEl.getBoundingClientRect();
+    const containerRect = containerEl.getBoundingClientRect();
+
+    this.selfCoord = {
+      x: nodeRect.left - containerRect.left + nodeRect.width / 2,
+      y: nodeRect.top - containerRect.top + nodeRect.height,
+    };
+
+    this.childCoords = this.childrenNodes.map((child) => {
+      const childRect = child.nodeRef.nativeElement.getBoundingClientRect();
+      return {
+        x: childRect.left - containerRect.left + childRect.width / 2,
+        y: childRect.top - containerRect.top,
+      };
+    });
   }
 }
